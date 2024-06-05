add9c4eb3ac33a29d739fd2085283f8be0320d47
Carlos Alberto Ribas <ribascarlos.a@outlook.com>
2024-06-05 16:54:52 -0300

    modificacion 07.js

---
+++ IntroduccionJS/rama-js/07.js
@@ -0,0 +1,15 @@
+/** Números**/
+
+
+const num1 = 100;
+const num2 = 200;
+const num3 = 203.23;
+const num4 = .3432;
+const num5 = -654;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+
