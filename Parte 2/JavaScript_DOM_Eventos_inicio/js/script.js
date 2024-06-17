74cae3e8411fd550cf0958f1f18b7e8f76b0755d
Carlos A. Ribas <ribascarlos.a@outlook.com>
2024-06-17 10:04:58 -0300

    Uso queryselector

---
+++ Parte 2/JavaScript_DOM_Eventos_inicio/js/script.js
@@ -0,0 +1,8 @@
+//QuerySelector
+const heading = document.querySelector('.header__texto h2');// retorna 0 o 1 elemento 
+console.log(heading);
+heading.textContent = 'Nuevo Heading'; 
+
+//QuerySelectorAll
+
+//getElementById
