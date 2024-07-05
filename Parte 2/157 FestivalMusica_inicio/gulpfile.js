011f805909cb2bbc074a8871ca0d8ae6114204a5
Carlos A. Ribas <ribascarlos.a@outlook.com>
2024-07-05 11:49:58 -0300

    Proyecto NodeJs

---
+++ Parte 2/157 FestivalMusica_inicio/gulpfile.js
@@ -0,0 +1,26 @@
+// importacion
+const { series, src, dest, watch } = require ('gulp'); //paquete gulp tiene mucho elementos (mutiples funciones), y con series import esa funcion
+const sass = require('gulp-sass')(require('sass')); //gulp-sass es una funcion 
+
+
+// funcion que compila SASS
+function css(  ) {
+    return src('src/scss/app.scss') // direccion de la hoja de estilo
+        .pipe( sass({}) ) // ejecucion de (funcion or paquete importado)
+        .pipe ( dest('./build/css') ) // guardar (tarea or la hoja de estilos or ...)
+}
+function minificarcss() {
+    return src('src/scss/app.scss')
+        .pipe( sass({
+            outputStyle : 'compressed'
+        }) ) 
+        .pipe ( dest('./build/css') )
+}
+function watchArchivos() {
+    watch('src/scss/**/*.scss',css ) // escucha los cambios (archivo) al cambiar, luego, ejecuta funcion css.
+} // * = la carpeta actual.- ** = todos los archivos con esa extensión.
+
+
+exports.css = css;
+exports.minificarcss = minificarcss;
+exports.watchArchivos = watchArchivos;
